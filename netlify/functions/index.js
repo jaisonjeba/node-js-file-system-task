4ba202f40b41ad3a73849043c070f2c3f3bf3b41
jaisonjeba <jaiprabakar5@gmail.com>
2024-08-31 22:10:30 +0530

    updated

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-const folderPath = "./Date-time";
+const folderPath = "../../Date-time";
 
 const PORT = 4002;
 app.get("/", (req, res) => {
